41497e4cd66702c371ac4f2be93f546b9b757ce6
gpc <gpc731@qq.com>
2023-06-07 15:01:33 +0800

    格式化代码

--- EasyTableML.py
+++ EasyTableML.py
@@ -106,8 +106,8 @@ class EasyTableMLRegression():
             os.makedirs(os.path.join('models', 'AutoML'))
         best_models = {}
         for i, (name, model) in enumerate(model_list.items()):
-            print('Note(Auto Parameter):', 'We are currently searching the ',
-                  i + 1, " model's beat parameter, ", 'model name: ', name)
+            print('Note(Auto Parameter):', 'We are currently searching the ', i + 1, " model's beat parameter, ",
+                  'model name: ', name)
             print('Plase Wait...')
             grid_search = GridSearchCV(model,
                                        parameters[name],
@@ -117,10 +117,8 @@ class EasyTableMLRegression():
                                        verbose=details,
                                        n_jobs=n_jobs)
             grid_search.fit(x_train, y_train)
-            joblib.dump(grid_search.best_estimator_,
-                        os.path.join('models', 'AutoML', name + '.pkl'))
-            print('Best model will be saved in:',
-                  os.path.join('models', 'AutoML', name + '.pkl'))
+            joblib.dump(grid_search.best_estimator_, os.path.join('models', 'AutoML', name + '.pkl'))
+            print('Best model will be saved in:', os.path.join('models', 'AutoML', name + '.pkl'))
             best_models[name] = grid_search.best_estimator_
         print('Auto Parameter Done!')
         return best_models
@@ -140,52 +138,43 @@ class EasyTableMLRegression():
         if train_type == 'base':
             if auto_parameter == False:
                 for i, (name, model) in enumerate(model_list.items()):
-                    print('Note:', 'We are currently training the ', i + 1,
-                          ' model,', 'model name: ', name)
+                    print('Note:', 'We are currently training the ', i + 1, ' model,', 'model name: ', name)
                     print('Plase Wait...')
                     model.fit(x_train, y_train)
                     #save model
                     joblib.dump(model, os.path.join('models', name + '.pkl'))
                 print('Train Done!')
             else:
-                model_list = self.base_auto_parameter(
-                    model_list,
-                    x_train,
-                    y_train,
-                    auto_scoring=auto_scoring,
-                    cv=cv,
-                    custom_parameters=auto_custom_parameters,
-                    n_jobs=n_jobs,
-                    details=details)
+                model_list = self.base_auto_parameter(model_list,
+                                                      x_train,
+                                                      y_train,
+                                                      auto_scoring=auto_scoring,
+                                                      cv=cv,
+                                                      custom_parameters=auto_custom_parameters,
+                                                      n_jobs=n_jobs,
+                                                      details=details)
             return model_list
         if train_type == 'meta':
             if auto_parameter == False:
                 if meta_model == None:
                     raise ValueError(
-                        'When you train META model and not use auto_parameter, "meta_model" cannot be None'
-                    )
+                        'When you train META model and not use auto_parameter, "meta_model" cannot be None')
                 print('Note:', 'We are currently training the META model')
                 print('Plase Wait...')
-                meta_learner = StackingRegressor(estimators=list(
-                    model_list.items()),
+                meta_learner = StackingRegressor(estimators=list(model_list.items()),
                                                  final_estimator=meta_model,
                                                  n_jobs=-1)
                 meta_learner.fit(x_train, y_train)
-                joblib.dump(meta_learner,
-                            os.path.join('models', 'meta_learner.pkl'))
+                joblib.dump(meta_learner, os.path.join('models', 'meta_learner.pkl'))
             else:
                 if auto_custom_parameters == None:
-                    meta_learner = StackingRegressor(
-                        estimators=list(model_list.items()),
-                        final_estimator=MLPRegressor(hidden_layer_sizes=(30,
-                                                                         100,
-                                                                         10),
-                                                     max_iter=5000,
-                                                     alpha=0.1),
-                        cv=5)
+                    meta_learner = StackingRegressor(estimators=list(model_list.items()),
+                                                     final_estimator=MLPRegressor(hidden_layer_sizes=(30, 100, 10),
+                                                                                  max_iter=5000,
+                                                                                  alpha=0.1),
+                                                     cv=5)
                 else:
-                    grid_search = GridSearchCV(StackingRegressor(
-                        estimators=list(model_list.items()), cv=5),
+                    grid_search = GridSearchCV(StackingRegressor(estimators=list(model_list.items()), cv=5),
                                                auto_custom_parameters,
                                                refit=True,
                                                cv=cv,
@@ -194,9 +183,7 @@ class EasyTableMLRegression():
                                                n_jobs=n_jobs)
                     grid_search.fit(x_train, y_train)
                     meta_learner = grid_search.best_estimator_
-                joblib.dump(
-                    meta_learner,
-                    os.path.join('models', 'AutoML', 'meta_learner.pkl'))
+                joblib.dump(meta_learner, os.path.join('models', 'AutoML', 'meta_learner.pkl'))
             return meta_learner
 
     def load_base_model(self, model_list, is_auto_parameter=False):
@@ -204,29 +191,23 @@ class EasyTableMLRegression():
             for i in range(len(model_list)):
                 model_ll = list(model_list.items())
                 name = model_ll[i][0]
-                print('Note:', 'Load model ', i + 1, ' ,', 'model name: ',
-                      name)
-                model_list[name] = joblib.load(
-                    os.path.join('models', name + '.pkl'))
+                print('Note:', 'Load model ', i + 1, ' ,', 'model name: ', name)
+                model_list[name] = joblib.load(os.path.join('models', name + '.pkl'))
             print('Load Done!')
         else:
             for i in range(len(model_list)):
                 model_ll = list(model_list.items())
                 name = model_ll[i][0]
-                print('Note:', 'Load model ', i + 1, ' ,', 'model name: ',
-                      name)
-                model_list[name] = joblib.load(
-                    os.path.join('models', 'AutoML', name + '.pkl'))
+                print('Note:', 'Load model ', i + 1, ' ,', 'model name: ', name)
+                model_list[name] = joblib.load(os.path.join('models', 'AutoML', name + '.pkl'))
             print('Load Done!')
         return model_list
 
     def load_meta_learner(self, is_auto_parameter=False):
         if is_auto_parameter == True:
-            meta_learner = joblib.load(
-                os.path.join('models', 'AutoML', 'meta_learner.pkl'))
+            meta_learner = joblib.load(os.path.join('models', 'AutoML', 'meta_learner.pkl'))
         else:
-            meta_learner = joblib.load(
-                os.path.join('models', 'meta_learner.pkl'))
+            meta_learner = joblib.load(os.path.join('models', 'meta_learner.pkl'))
         return meta_learner
 
     def estimate(self, model_list, x_test, y_test, details=0):
@@ -259,20 +240,12 @@ class EasyTableMLRegression():
         result_score.columns = cols
         return P, result_score
 
-    def estimate_base(self,
-                      model_list,
-                      x_train,
-                      y_train,
-                      x_valid,
-                      y_valid,
-                      details=0):
+    def estimate_base(self, model_list, x_train, y_train, x_valid, y_valid, details=0):
         print('Estimating Train Set...')
-        _, result_score_train = self.estimate(model_list, x_train, y_train,
-                                              details)
+        _, result_score_train = self.estimate(model_list, x_train, y_train, details)
 
         print('Estimating Valid/Test Set...')
-        P, result_score_valid = self.estimate(model_list, x_valid, y_valid,
-                                              details)
+        P, result_score_valid = self.estimate(model_list, x_valid, y_valid, details)
 
         if (details >= 1):
             print('result_score_train')
@@ -286,15 +259,11 @@ class EasyTableMLRegression():
         low_score = result_score_valid.T
         low_score = low_score[low_score['R2'] < 0.9]
         if (len(overfit) > 0):
-            print(
-                'The fellowing models may have overfitting, please consider modifying them.'
-            )
+            print('The fellowing models may have overfitting, please consider modifying them.')
             print('Train score - Valid score:')
             print(overfit)
         if (len(low_score) > 0):
-            print(
-                'The fellowing models have lower scores, please consider removing them.'
-            )
+            print('The fellowing models have lower scores, please consider removing them.')
             print(low_score)
         if (len(overfit) == 0 and len(low_score) == 0):
             print('All models performed well!')
@@ -305,20 +274,12 @@ class EasyTableMLRegression():
         sns.heatmap(corrmat, square=True, annot=True)
         return P
 
-    def estimate_meta(self,
-                      meta_model,
-                      x_train,
-                      y_train,
-                      x_valid,
-                      y_valid,
-                      details=0):
+    def estimate_meta(self, meta_model, x_train, y_train, x_valid, y_valid, details=0):
         print('Estimating Train Set...')
-        _, result_score_train = self.estimate({'meta_model': meta_model},
-                                              x_train, y_train, details)
+        _, result_score_train = self.estimate({'meta_model': meta_model}, x_train, y_train, details)
 
         print('Estimating Valid/Test Set...')
-        _, result_score_valid = self.estimate({'meta_model': meta_model},
-                                              x_valid, y_valid, details)
+        _, result_score_valid = self.estimate({'meta_model': meta_model}, x_valid, y_valid, details)
 
         if (details >= 1):
             print('result_score_train')
@@ -332,9 +293,7 @@ class EasyTableMLRegression():
         low_score = result_score_valid.T
         low_score = low_score[low_score['R2'] < 0.9]
         if (len(overfit) > 0):
-            print(
-                'The model may have overfitting, please consider modifying it.'
-            )
+            print('The model may have overfitting, please consider modifying it.')
             print('Train score - Valid score:')
             print(overfit)
         if (len(low_score) > 0):
@@ -347,13 +306,7 @@ class EasyTableMLRegression():
         y_pred = model.predict(x_pred)
         return y_pred
 
-    def fit(self,
-            x_train,
-            y_train,
-            auto_scoring='r2',
-            cv=3,
-            n_jobs=1,
-            details=1):
+    def fit(self, x_train, y_train, auto_scoring='r2', cv=3, n_jobs=1, details=1):
         print('Start Auto Train! Leave it all to me 😊!')
         base_models = self.get_base_models()
         best_models = self.train(base_models,
